513e57ecaef36527436424fa1ddd8688b55d5d83
Omar Diab <me@omardiab.com>
2020-05-17 20:15:48 +0900

    add tests from io-ts-reporters

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,14 +1,58 @@
 import test from 'ava';
 import * as iots from 'io-ts';
 
-import reporter from 'src';
+import {reporter} from 'src';
 
-test('it handles wrong value type for interface', t => {
-	const codec = iots.interface({a: iots.string});
-	const report = reporter.report(codec.decode({a: 5}));
-	t.deepEqual(report, ['a: something was wrong']);
+test('reports an empty array when the result doesn’t contain errors', t => {
+	const PrimitiveType = iots.string;
+	const result = PrimitiveType.decode('');
+
+	t.deepEqual(reporter.report(result), []);
 });
-test('it decodes', t => {
-	const codec = iots.interface({a: iots.string, b: iots.interface({b1: iots.number})});
-	t.is(reporter.report(codec.decode({a: 5, b: {b1: 'yo'}})), ['hi']);
+
+test('formats a top-level primitve type correctly', t => {
+	const PrimitiveType = iots.string;
+	const result = PrimitiveType.decode(42);
+
+	t.deepEqual(reporter.report(result), ['Expecting string but instead got: 42']);
+});
+
+test('formats array items', t => {
+	const NumberGroups = iots.array(iots.array(iots.number));
+	const result = NumberGroups.decode({});
+
+	t.deepEqual(reporter.report(result), ['Expecting Array<Array<number>> but instead got: {}']);
+});
+
+test('formats nested array item mismatches correctly', t => {
+	const NumberGroups = iots.array(iots.array(iots.number));
+	const result = NumberGroups.decode([[{}]]);
+
+	t.deepEqual(reporter.report(result), ['Expecting number at 0.0 but instead got: {}']);
+});
+
+test('formats a complex type correctly', t => {
+	const Gender = iots.union([iots.literal('Male'), iots.literal('Female')]);
+	const Person = iots.interface({
+		name: iots.string,
+		age: iots.number,
+		gender: Gender,
+		children: iots.array(
+			iots.interface({
+				gender: Gender
+			})
+		)
+	});
+	const result = Person.decode({
+		name: 'Giulio',
+		children: [{gender: 'Whatever'}]
+	});
+
+	t.deepEqual(reporter.report(result), [
+		'Expecting number at age but instead got: undefined',
+		'Expecting "Male" at gender.0 but instead got: undefined',
+		'Expecting "Female" at gender.1 but instead got: undefined',
+		'Expecting "Male" at children.0.gender.0 but instead got: "Whatever"',
+		'Expecting "Female" at children.0.gender.1 but instead got: "Whatever"'
+	]);
 });
