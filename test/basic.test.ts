cd7d809129775be84fd0b92a0687ddcf5ede34ca
Omar Diab <me@omardiab.com>
2020-05-17 20:18:37 +0900

    prettier style on tests and new formatting

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -3,35 +3,41 @@ import * as iots from 'io-ts';
 
 import {reporter} from 'src';
 
-test('reports an empty array when the result doesn’t contain errors', t => {
+test('reports an empty array when the result doesn’t contain errors', (t) => {
 	const PrimitiveType = iots.string;
 	const result = PrimitiveType.decode('');
 
 	t.deepEqual(reporter.report(result), []);
 });
 
-test('formats a top-level primitve type correctly', t => {
+test('formats a top-level primitve type correctly', (t) => {
 	const PrimitiveType = iots.string;
 	const result = PrimitiveType.decode(42);
 
-	t.deepEqual(reporter.report(result), ['Expecting string but instead got: 42']);
+	t.deepEqual(reporter.report(result), [
+		'Expecting value of type "string" but instead got value: 42'
+	]);
 });
 
-test('formats array items', t => {
+test('formats array items', (t) => {
 	const NumberGroups = iots.array(iots.array(iots.number));
 	const result = NumberGroups.decode({});
 
-	t.deepEqual(reporter.report(result), ['Expecting Array<Array<number>> but instead got: {}']);
+	t.deepEqual(reporter.report(result), [
+		'Expecting value of type "Array<Array<number>>" but instead got value: {}'
+	]);
 });
 
-test('formats nested array item mismatches correctly', t => {
+test('formats nested array item mismatches correctly', (t) => {
 	const NumberGroups = iots.array(iots.array(iots.number));
 	const result = NumberGroups.decode([[{}]]);
 
-	t.deepEqual(reporter.report(result), ['Expecting number at 0.0 but instead got: {}']);
+	t.deepEqual(reporter.report(result), [
+		'Expecting value of type "number" at path "0.0" but instead got value: {}'
+	]);
 });
 
-test('formats a complex type correctly', t => {
+test('formats a complex type correctly', (t) => {
 	const Gender = iots.union([iots.literal('Male'), iots.literal('Female')]);
 	const Person = iots.interface({
 		name: iots.string,
@@ -49,10 +55,10 @@ test('formats a complex type correctly', t => {
 	});
 
 	t.deepEqual(reporter.report(result), [
-		'Expecting number at age but instead got: undefined',
-		'Expecting "Male" at gender.0 but instead got: undefined',
-		'Expecting "Female" at gender.1 but instead got: undefined',
-		'Expecting "Male" at children.0.gender.0 but instead got: "Whatever"',
-		'Expecting "Female" at children.0.gender.1 but instead got: "Whatever"'
+		'Expecting value of type "number" at path "age" but instead got value: undefined',
+		'Expecting value of type ""Male"" at path "gender.0" but instead got value: undefined',
+		'Expecting value of type ""Female"" at path "gender.1" but instead got value: undefined',
+		'Expecting value of type ""Male"" at path "children.0.gender.0" but instead got value: "Whatever"',
+		'Expecting value of type ""Female"" at path "children.0.gender.1" but instead got value: "Whatever"'
 	]);
 });
